2e81cffe0999cdffb36879d9614d51d8c9719ef8
TahaAlTamimi <altamimitmo@gmail.com>
2019-07-25 15:38:33 +0300

    add timer update 2

--- JS Code.js
+++ JS Code.js
@@ -24,23 +24,36 @@ var cth=0;
     ctm=0;
     cts=0;
     start=0;
-function countdownTimer(){
+    var timer_is_on = 0;
+
+    var control;
+function down(){
+    
 if(start===0&&document.getElementById("minute")&&document.getElementById("second")){
 ctm=parseInt(document.getElementById("minute").value);
 cts=parseInt(document.getElementById("second").value);
-if(isNaN(cth)) cth=0;
-if(isNaN(ctm)) ctm=0;
-if(isNaN(cts)) cts=0;
-document.getElementById("minute").value=cth;
-document.getElementById("second").value=ctm;
+// if(isNaN(cth)) cth=0;
+// if(isNaN(ctm)) ctm=0;
+// if(isNaN(cts)) cts=0;
+document.getElementById("minute").value=ctm;
+document.getElementById("second").value=cts;
 start=1;
 // document.getElementById('btn').setAttribute('disabled', 'disabled'); 
 }
 if(ctm===0 && cts===0) {
     start = 0;
-    document.getElementById('btn').removeAttribute('disabled');
+   // document.getElementById('btn').removeAttribute('disabled');
     return false;
 }else{
+
+if(ctm>60)
+ctm=60;
+document.getElementById("minute").value=ctm;
+
+if(cts>60)
+cts=60;
+document.getElementById("second").value=cts;
+
     cts--;
     if(cts<0){
         if(ctm>0){
@@ -53,28 +66,82 @@ if(ctm===0 && cts===0) {
               
             }
         }
-    }
 
- 
+
+
+
+
+    }
+    
+    
 document.getElementById('showmns').innerHTML = ctm;
   document.getElementById('showscs').innerHTML = cts;
-  setTimeout('countdownTimer()', 1000);
+   control= setTimeout('down()', 1000);
+
+/////////////////////
+}
+
+function startTimer(){
+  
+  down();
+  document.getElementById('btn1').setAttribute('disabled', 'disabled');
+
+  document.getElementById('btn2').disabled= false;
+
+  }
+
+
+function stopTimer(){
+  //  document.getElementById('btn2')
+
+    clearTimeout(control);
+    document.getElementById('btn2').setAttribute('disabled', 'disabled');
+
+    document.getElementById('btn1').disabled= false;
+
 }
 
 
+function Reset(){
+    //  document.getElementById('btn2')
+    document.getElementById('showmns').innerHTML = 0;
+    document.getElementById('showscs').innerHTML = 0;
+   document.getElementById('minute').value = 0;
+   document.getElementById('second').value = 0;
+   var cth=0;
+    ctm=0;
+    cts=0;
+    start=0;
+    var timer_is_on = 0;
 
-$(document).ready(function(){
-    $("#btn").on("click",function(){
-        var x = $('#btn').val()
-        if (x === 'Start') {
-            $('#btn').attr('value', 'Stop')
-            
-        } else {
-            $('#btn').attr('value', 'Start')
-        }
-    })
+    var control;
+    document.getElementById('btn1').disabled= false;
+
+  }
+
+
+
+// $(document).ready(function(){
+
     
-    })
+//     $("#btn").on("click",function(){
+
+        
+//         var x = $('#btn').val()
+//         if (x === 'Start') {
+//             $('#btn').attr('value', 'Stop')
+            
+//         } else {
+//             $('#btn').attr('value', 'Start')
+//         }
+
+
+        
+//     })
+    
+//     });
+    
+   
     // to display button start
     // if(cth===0){
     // document.getElementById('btn').setAttribute('disabled', 'disabled');
